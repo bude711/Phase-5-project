1cbe4e540f11d662af9e9e28d0c388a9ea0ffb79
+ <brandi.ude@gmail.com>
2022-01-14 14:37:30 -0500

    set up back end user and sessions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+    skip_before_action :authorize, only: :create
+
+  def create
+    user = User.create!(user_params)
+    session[:user_id] = user.id
+    render json: user, status: :created
+  end
+
+  def show
+    render json: @current_user
+  end
+
+  def destroy 
+    user = find_user
+    user.destroy 
+    head :no_content
+end
+
+def update 
+    user = find_user 
+    user.update!(user_params)
+    render json: user 
+end
+
+  private
+
+  def user_params
+    params.permit(:username, :password, :password_confirmation, :first_name, :last_name, :email)
+  end
+
+  def find_user 
+    User.find(params[:id])
+end
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+
+    has_secure_password
+
+    validates :username, presence: true, uniqueness: true
+    validates :first_name, presence: true
+    validates :last_name, presence: true 
+    validates :email, presence: true, uniqueness: true
+
+end
